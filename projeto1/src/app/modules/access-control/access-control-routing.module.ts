dd0fec7874978f39098e0b97cd517a9bd3d563dc
Eder Martins Franco <efranco23@gmail.com>
2019-08-22 01:37:43 -0400

    Adicionando bootstrap

--- projeto1/src/app/modules/access-control/access-control-routing.module.ts
+++ projeto1/src/app/modules/access-control/access-control-routing.module.ts
@@ -2,10 +2,11 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
