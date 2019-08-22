62e7c6b82461bab1a8c22d3d1663e1aa082c7c22
Eder Martins Franco <efranco23@gmail.com>
2019-08-21 23:24:30 -0400

    WIP: login

--- projeto1/src/app/modules/access-control/access-control.module.ts
+++ projeto1/src/app/modules/access-control/access-control.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ]
 })
 export class AccessControlModule { }
--- projeto1/src/app/modules/access-control/login/login.component.ts
+++ projeto1/src/app/modules/access-control/login/login.component.ts
@@ -1,3 +1,4 @@
+import { LoginModel } from './../login.model';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,14 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public data: LoginModel;
+
   constructor(private loginService: AuthService) { }
 
   ngOnInit() {
+    this.data = new LoginModel();
+    // this.data.username = 'eder.franco';
   }
 
   public send() {
     // this.loginService.login()
-    alert('Oi');
+    console.log(this.data);
+  }
+
+  public isValid() {
+    return this.data.username && this.data.password;
   }
 
 }
